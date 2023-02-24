defa79dac8ec2a5b3af33432f2e7a48eff16a628
Romain Andres <romain.andres@etu.unicaen.fr>
2023-02-24 23:59:39 +0100

    Amelioration du script

--- script.sh
+++ script.sh
@@ -1,4 +1,7 @@
 java org.antlr.v4.Tool Calculette.g4 
+java org.antlr.v4.Tool MVaP.g4 
 javac *.java
-java MainCalculette > test.mvap
-cat test.mvap
+java -Xmx500M -cp "/usr/share/java/antlr-4.11.1-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig Calculette 'start' -gui > test.mvap
+javac MVaPAssembler.java CBaP.java
+java MVaPAssembler -d test.mvap
+java CBaP -d test.mvap.cbap
